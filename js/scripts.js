1fba3877ceff8b686f6c55f9aeb585ab686fcd43
Kelli Brooke and Renee Sarley <renee@>
2018-05-30 16:56:33 -0700

    initial and final commit oopsies

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(function(){
+
+var calc = "";
+var add = "+";
+var subtract = "-";
+var multiply = "*";
+var divide = "/";
+
+	$("button.num").click(function(){
+    var idVar= $(this).attr("id");
+  	calc= calc + idVar;
+    $("#window").text(calc);
+  });
+  $("#plus").click(function(){
+    calc= calc + add;
+    $("#window").text(calc);
+  });
+  $("#minus").click(function(){
+    calc= calc + subtract;
+    $("#window").text(calc);
+  });
+  $("#times").click(function(){
+    calc= calc + multiply;
+    $("#window").text(calc);
+  });
+  $("#dividedBy").click(function(){
+    calc= calc + divide;
+    $("#window").text(calc);
+  });
+
+  $("#equals").click(function(){
+  	calc =eval(calc);
+  	 $("#window").text(calc);
+
+  });
+  $("#clear").click(function(){
+    calc ="";
+    $("#window").text(calc);
+
+  });
+
+});
