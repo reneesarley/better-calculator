5cb7f06f4270d90179bb400d314f2deb7936d2cf
Renee Sarley <reneesarley@gmail.com>
2018-06-02 13:51:10 -0700

    add one function that will work for all opperator buttons

--- js/scripts.js
+++ js/scripts.js
@@ -11,32 +11,38 @@ var divide = "/";
   	calc= calc + idVar;
     $("#window").text(calc);
   });
-  $("#plus").click(function(){
-    calc= calc + add;
-    $("#window").text(calc);
-  });
-  $("#minus").click(function(){
-    calc= calc + subtract;
-    $("#window").text(calc);
-  });
-  $("#times").click(function(){
-    calc= calc + multiply;
-    $("#window").text(calc);
-  });
-  $("#dividedBy").click(function(){
-    calc= calc + divide;
-    $("#window").text(calc);
-  });
-
-  $("#equals").click(function(){
-  	calc =eval(calc);
-  	 $("#window").text(calc);
-
-  });
-  $("#clear").click(function(){
-    calc ="";
-    $("#window").text(calc);
-
-  });
+	$("button.opperator").click(function(){
+		var idVar= $(this).attr("id");
+		alert(idVar);
+		calc= calc + eval(idVar);
+		$("#window").text(calc);
+	});
+  // $("#plus").click(function(){
+  //   calc= calc + add;
+  //   $("#window").text(calc);
+  // });
+  // $("#minus").click(function(){
+  //   calc= calc + subtract;
+  //   $("#window").text(calc);
+  // });
+  // $("#times").click(function(){
+  //   calc= calc + multiply;
+  //   $("#window").text(calc);
+  // });
+  // $("#dividedBy").click(function(){
+  //   calc= calc + divide;
+  //   $("#window").text(calc);
+  // });
+	//
+  // $("#equals").click(function(){
+  // 	calc =eval(calc);
+  // 	 $("#window").text(calc);
+	//
+  // });
+  // $("#clear").click(function(){
+  //   calc ="";
+  //   $("#window").text(calc);
+	//
+  // });
 
 });
