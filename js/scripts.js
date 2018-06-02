f335ea4b3fd35dae893b8acf0d96308d8f2ebe65
Renee Sarley <reneesarley@gmail.com>
2018-06-02 13:55:57 -0700

    remove unused functions and debugger alerts

--- js/scripts.js
+++ js/scripts.js
@@ -13,36 +13,18 @@ var divide = "/";
   });
 	$("button.opperator").click(function(){
 		var idVar= $(this).attr("id");
-		alert(idVar);
 		calc= calc + eval(idVar);
 		$("#window").text(calc);
 	});
-  // $("#plus").click(function(){
-  //   calc= calc + add;
-  //   $("#window").text(calc);
-  // });
-  // $("#minus").click(function(){
-  //   calc= calc + subtract;
-  //   $("#window").text(calc);
-  // });
-  // $("#times").click(function(){
-  //   calc= calc + multiply;
-  //   $("#window").text(calc);
-  // });
-  // $("#dividedBy").click(function(){
-  //   calc= calc + divide;
-  //   $("#window").text(calc);
-  // });
-	//
-  // $("#equals").click(function(){
-  // 	calc =eval(calc);
-  // 	 $("#window").text(calc);
-	//
-  // });
-  // $("#clear").click(function(){
-  //   calc ="";
-  //   $("#window").text(calc);
-	//
-  // });
+
+  $("#equals").click(function(){
+  	calc =eval(calc);
+  	 $("#window").text(calc);
+  });
+
+  $("#clear").click(function(){
+    calc ="";
+    $("#window").text(calc);
+  });
 
 });
